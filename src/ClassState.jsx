c47d02db2b5ecbeb993141533226cad4ed7b6d29
Mauricio J Carrillo <mauricio.jcarrillo@gmail.com>
2022-06-20 15:48:39 -0500

    Clase 3: React.Component vs. useState

--- src/ClassState.jsx
+++ src/ClassState.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 
 export class ClassState extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: false,
+    };
+  }
   render() {
     return (
       <div>
-        <h2>Eliminar ClassState</h2>
+        <h2>Eliminar {this.props.name}</h2>
 
         <p>Por favor escribe el código de seguridad</p>
+        {this.state.error && <p>Error: el código es incorrecto</p>}
 
         <input placeholder="Código de seguridad" />
-        <button>Comprobar</button>
+        <button onClick={() => this.setState({ error: !this.state.error })}>
+          Comprobar
+        </button>
       </div>
     );
   }
